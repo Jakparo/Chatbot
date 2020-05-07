0a620d540d0dafac85ed2bb0dee20fb32d6036a6
Jackparo <zingme321@gmail.com>
2020-05-07 22:49:26 +0700

    port

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ app.use(express.static("public"));
 
 app.set("view engine", "pug");
 app.set("views", "./views");
-
+var port = process.env.PORT || 3000;
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log("Server is available now")
 });
 
